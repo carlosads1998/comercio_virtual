cce01e13a806d85cfb06e2b3526c160ebb6f3d5d
carlosads1998 <carlos.ads1998@gmail.com>
2022-02-24 17:35:05 -0300

    pronto para deploy

--- ldc/settings.py
+++ ldc/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = '&on0#gonu&jr7-y@042mfmu94i6j#k6o$!9o5q7uy)i!@+*=x_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://comercio-ldc.herokuapp.com/']
 
 
 # Application definition
@@ -135,3 +136,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+django_heroku.settings(locals())
