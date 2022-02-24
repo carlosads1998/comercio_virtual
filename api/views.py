0264f72f6889535303d2afcbcb21ad8aea5416dd
carlosads1998 <carlos.ads1998@gmail.com>
2022-02-24 19:30:59 -0300

    atual

--- api/views.py
+++ api/views.py
@@ -4,8 +4,8 @@ from .serializers import lojaSerializer
 from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticated
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework_simplejwt.views import TokenObtainPairView
+#from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+#from rest_framework_simplejwt.views import TokenObtainPairView
 
 class lojaAPIView(generics.ListCreateAPIView):
     permission_classes = (IsAuthenticated, )
@@ -21,16 +21,16 @@ class lojasAPIView(generics.RetrieveUpdateDestroyAPIView):
     filter_backends = [DjangoFilterBackend]
     filter_fields = ['nome']
     
-class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-    @classmethod
-    def get_token(cls, user):
-        token = super().get_token(user)
+#class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+#    @classmethod
+#    def get_token(cls, user):
+#        token = super().get_token(user)
+#
+#        # Add custom claims
+#        token['name'] = user.name
+#        ...
+#
+#        return token
 
-        # Add custom claims
-        token['name'] = user.name
-        # ...
-
-        return token
-
-class MyTokenObtainPairView(TokenObtainPairView):
-    serializer_class = MyTokenObtainPairSerializer
+#class MyTokenObtainPairView(TokenObtainPairView):
+#    serializer_class = MyTokenObtainPairSerializer
