c3f97c7c0321461570cbf652a42717771b71c798
carlosads1998 <carlos.ads1998@gmail.com>
2022-02-24 19:28:37 -0300

    atual

--- api/views.py
+++ api/views.py
@@ -3,10 +3,12 @@ from .models import loja
 from .serializers import lojaSerializer
 from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
-#from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-#from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 class lojaAPIView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated, )
     queryset = loja.objects.all()
     serializer_class = lojaSerializer
     filter_backends = [DjangoFilterBackend]
@@ -19,16 +21,16 @@ class lojasAPIView(generics.RetrieveUpdateDestroyAPIView):
     filter_backends = [DjangoFilterBackend]
     filter_fields = ['nome']
     
-#class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-#    @classmethod
-#    def get_token(cls, user):
-#        token = super().get_token(user)
-#
-#        # Add custom claims
-#        token['name'] = user.name
-#        # ...
-#
-#        return token
-#
-#class MyTokenObtainPairView(TokenObtainPairView):
-#    serializer_class = MyTokenObtainPairSerializer
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['name'] = user.name
+        # ...
+
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
--- ldc/settings.py
+++ ldc/settings.py
@@ -103,15 +103,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-#REST_FRAMEWORK = {
-#    'DEFAULT_PERMISSION_CLASSES':(
-#        'rest_framework.permissions.IsAuthenticated',
-#    ),
-#    'DEFAULT_AUTHENTICATION_CLASSES':(
-#        'rest_framework_simplejwt.authentication.JWTAuthentication',
-#        'rest_framework.authentication.SessionAuthentication',
-#    ),
-#}
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES':(
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES':(
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+}
 
 
 
