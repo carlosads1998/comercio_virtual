5857676c81d681b97b275941957532277a1c1da7
carlosads1998 <carlos.ads1998@gmail.com>
2022-02-24 19:09:41 -0300

    atual

--- ldc/urls.py
+++ ldc/urls.py
@@ -19,7 +19,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-#from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
